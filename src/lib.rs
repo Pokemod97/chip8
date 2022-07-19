2996f103afb1eb83d695ad22af6a0947f275ab69
Pokemod97 <cadenkline9@gmail.com>
2022-07-18 20:08:02 -0400

    chip8 done

---
+++ src/lib.rs
@@ -0,0 +1,260 @@
+use std::path::Path;
+use std::fs;
+use std::time::{Instant, Duration};
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+
+pub struct Chip8 {
+    memory: [u8; 0xFFF],
+    register_i: u16,
+    registers: [u8; 16],
+    pc: u16,
+    sp: u8,
+    stack: [u16; 16],
+    screen: [[bool; WIDTH]; HEIGHT],
+    dt: u8,
+    st: u8,
+}
+
+impl Default for Chip8 {
+    fn default() -> Self {
+        Chip8 {
+            memory: [0; 0xFFF],
+            register_i: 0,
+            registers: [0; 16],
+            pc: 0x200,
+            sp: 0,
+            stack: [0; 16],
+            screen: [[false; WIDTH]; HEIGHT],
+            dt: 0,
+            st: 0,
+        }
+    }
+}
+
+impl Chip8 {
+    pub fn run_instruction(&mut self, key: &mut Option<u8>, time: &mut Instant) {
+        if self.pc >= 0xFFE {
+            return;
+        }
+
+        let instruction = (self.memory[self.pc as usize] as u16) << 8
+            | self.memory[(self.pc + 1) as usize] as u16;
+        //println!("{:x}, {:x}", instruction, self.pc);
+        let kk = (0xFF & instruction) as u8;
+        let adr = 0xFFF & instruction;
+        let n = 0xF & instruction;
+        let y = self.registers[((instruction & 0x00F0) >> 4) as usize];
+        let v0 = self.registers[0];
+        let x = &mut self.registers[((instruction & 0x0F00) >> 8) as usize];
+        
+        match instruction & 0xF000 {
+            0 => {
+                if kk == 0xEE {
+                    self.pc = self.stack[self.sp as usize];
+                    self.sp -= 1;
+                    return
+                } else if kk == 0xE0 {
+                    self.screen.copy_from_slice(&[[false; 64]; 32]);
+                }
+            }
+            0x1000 => {self.pc = adr; return},
+            0x2000 => {
+                self.sp += 1;
+                self.stack[self.sp as usize] = self.pc +2;
+                self.pc = adr;
+                return;
+            }
+            0x3000 => {
+                if self.registers[((instruction & 0x0F00) >> 8) as usize] == kk {
+                    self.pc += 2;
+                }
+            }
+            0x4000 => {
+                if *x != kk {
+                    self.pc += 2;
+                }
+            }
+            0x5000 => {
+                if *x == y {
+                    self.pc += 2;
+                }
+            }
+            0x6000 => *x = kk,
+            0x7000 => *x = x.overflowing_add(kk).0,
+            0x8000 => match n {
+                0 => *x = y,
+                1 => *x |= y,
+                2 => *x &= y,
+                3 => *x ^= y,
+                4 => {
+                    let result = x.overflowing_add(y);
+                    *x = result.0;
+                    self.registers[0xF] = result.1 as u8;
+                }
+                5 => {
+                    let result = x.overflowing_sub(y);
+                    *x = result.0;
+                    self.registers[0xF] = !result.1 as u8;
+                }
+                0x6 =>{
+                    let end_bit = y & 1;
+                    *x = y>>1;
+                    self.registers[0xF] = end_bit;
+                } ,
+                0x7 => {
+                    let result = x.overflowing_sub(y);
+                    *x = result.0;
+                    self.registers[0xF] = result.1 as u8;
+                }
+                0xE => {
+                    let end_bit = (y >> 7) & 1;
+                    *x = y << 1;
+                    println!("{:x}, {:x}, {:x}",y,*x,end_bit);
+                    self.registers[0xF] = end_bit;
+                }
+                _ => (),
+            },
+            0x9000 => {
+                if *x != y {
+                    self.pc += 2;
+                }
+            }
+            0xA000 => {
+                self.register_i = adr;
+            },
+            0xB000 => {self.pc = adr + v0 as u16; return},
+            0xC000 => *x = kk & fastrand::u8(0..=255),
+            0xD000 => {
+                let x = (*x % 64) as usize;
+                let y = (y % 32) as usize;
+                let mut overwrite = false;
+                for i in 0..n as usize {
+                    let byte = self.memory[self.register_i as usize + i];
+
+
+                    for q in 0..8 as usize{
+                        let bit = ((byte >> q.abs_diff(7)) & 1) != 0;
+                        
+                        match self.screen.get_mut(y + i) {
+                            Some(u) => match u.get_mut(x + q) {
+                                Some(handle) => {
+                                    overwrite |= *handle ^ bit;
+                                    *handle ^= bit;
+                                    //println!("{handle},{bit}, {}, {}", y+i,x+q);
+                                }
+                                None => (),
+                            },
+                            None => (),
+                        };
+                    }
+                }
+                self.registers[0xF] = overwrite as u8;
+            }
+            0xE000 => match kk {
+                0x9E => {
+                    if *key != None && *x == key.unwrap() {
+                        self.pc += 2;
+                        *key = None;
+                    }
+                }
+                0xA1 => {
+                    match *key {
+                        Some(k) =>{
+                            if *x != k {
+                                self.pc += 2;
+                                *key = None;
+                            }
+                        },
+                        None => self.pc += 2,
+                    }
+                    
+                }
+                _ => (),
+            },
+            0xF000 => match kk {
+                0x07 => *x = self.dt,
+                0x0A => {
+                    if None == *key {
+                        return;
+                    } else {
+                        *x = key.unwrap();
+                        *key = None;
+                    }
+                }
+                0x15 => self.dt = *x,
+                0x18 => self.st = *x,
+                0x1E => self.register_i += *x as u16,
+                0x29 => self.register_i = 5 * *x as u16,
+                0x33 => {
+                    self.memory[(self.register_i) as usize] = *x / 100;
+                    self.memory[(self.register_i + 1) as usize] = (*x % 100) / 10;
+                    self.memory[(self.register_i + 2) as usize] = *x % 10;
+                }
+                0x55 => {
+                    let section = &self.registers[0..=((instruction & 0x0F00) >> 8) as usize];
+                    for (i, k) in section.iter().enumerate() {
+                        self.memory[(self.register_i as usize) + i] = *k;
+                    }
+                }
+                0x65 => {
+                    let section = &mut self.registers[0..=((instruction & 0x0F00) >> 8) as usize];
+                    for (i, k) in section.iter_mut().enumerate() {
+                        *k = self.memory[(self.register_i as usize) + i];
+                    }
+                }
+                _ => (),
+            },
+
+            _ => (),
+        }
+        if time.elapsed() >= Duration::new(0, 16666667) {
+            if self.st > 0 {
+                self.st -= 1;
+            }
+            if self.dt > 0 {
+                self.dt -= 1;
+            }
+            *time = Instant::now();
+        }
+
+        self.pc += 2;
+    }
+    pub fn draw(&self) -> Vec<u8> {
+        let mut pixels: Vec<u8> = Vec::new();
+        for y in self.screen {
+            for x in y {
+                if x {
+                    pixels.push(0xFF);
+                    pixels.push(0xFF);
+                    pixels.push(0xFF);
+                    pixels.push(0xFF);
+                } else {
+                    pixels.push(0x0);
+                    pixels.push(0xF0);
+                    pixels.push(0);
+                    pixels.push(0xFF);
+                }
+            }
+        }
+        pixels
+    }
+    pub fn setup(path: &Path) -> Chip8 {
+        let mut chip8: Chip8 = Chip8::default();
+        let font: [u8; 80] = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70, 0xF0, 0x10, 0xF0, 0x80,
+            0xF0, 0xF0, 0x10, 0xF0, 0x10, 0xF0, 0x90, 0x90, 0xF0, 0x10, 0x10, 0xF0, 0x80, 0xF0,
+            0x10, 0xF0, 0xF0, 0x80, 0xF0, 0x90, 0xF0, 0xF0, 0x10, 0x20, 0x40, 0x40, 0xF0, 0x90,
+            0xF0, 0x90, 0xF0, 0xF0, 0x90, 0xF0, 0x10, 0xF0, 0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0,
+            0x90, 0xE0, 0x90, 0xE0, 0xF0, 0x80, 0x80, 0x80, 0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0,
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80,
+        ];
+        chip8.memory[..80].copy_from_slice(&font);
+        let bytes = fs::read(path).expect("failed to read file") ;
+        //let bytes = vec![0x0,0xE0, 0x60, 0x0F, 0xF0, 0x29, 0xD1, 0x15];
+        chip8.memory[0x200..(0x200 + bytes.len())].copy_from_slice(&bytes);
+        //chip8.memory[0x1FF] = 5;
+        chip8
+        
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,115 @@
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
+use std::collections::HashMap;
+use std::path::Path;
+use std::time::{Duration, Instant};
+use chip8::Chip8;
+use once_cell::unsync::Lazy;
+
+use log::error;
+
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+const WIDTH: usize = 360;
+const HEIGHT: usize = 180;
+
+fn main() -> Result<(), Error> {
+    let keys = Lazy::new(|| {
+        let mut hash_map: HashMap<VirtualKeyCode, u8> = HashMap::new();
+        
+        hash_map.insert(VirtualKeyCode::Key1, 1);
+        hash_map.insert(VirtualKeyCode::Key2, 2);
+        hash_map.insert(VirtualKeyCode::Key3, 3);
+        hash_map.insert(VirtualKeyCode::Key4, 0xC);
+        hash_map.insert(VirtualKeyCode::Q, 4);
+        hash_map.insert(VirtualKeyCode::W, 5);
+        hash_map.insert(VirtualKeyCode::E, 6);
+        hash_map.insert(VirtualKeyCode::R, 0xD);
+        hash_map.insert(VirtualKeyCode::A, 7);
+        hash_map.insert(VirtualKeyCode::S, 8);
+        hash_map.insert(VirtualKeyCode::D, 9);
+        hash_map.insert(VirtualKeyCode::F, 0xE);
+        hash_map.insert(VirtualKeyCode::Z, 0xA);
+        hash_map.insert(VirtualKeyCode::X, 0x0);
+        hash_map.insert(VirtualKeyCode::C, 0xB);
+        hash_map.insert(VirtualKeyCode::V, 0xF);
+        return hash_map;
+    });
+
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(64 as f64, 32 as f64);
+        WindowBuilder::new()
+            .with_title("Hello Pixels")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(64 as u32, 32 as u32, surface_texture)?
+    };
+    let mut key: Option<u8> = None;
+    let mut chip8: Chip8 = Chip8::setup(&Path::new("chip8-test-suite.ch8"));
+    let mut instructions:u32 = 0;
+    let mut time = Instant::now();
+    let mut other_time = Instant::now();
+    event_loop.run(move |event, _, control_flow| {
+        // Draw the current frame
+        if instructions < 700001 {
+            chip8.run_instruction(&mut key, &mut other_time);
+            instructions += 1;
+        }
+        if time.elapsed() >= Duration::new(1,0){
+            instructions = 0;
+            time = Instant::now();
+        }
+        if let Event::RedrawRequested(_) = event {
+
+            
+            let buffer: Vec<u8> = chip8.draw();
+
+            pixels.get_frame().copy_from_slice(&buffer);
+            if pixels
+                .render()
+                .map_err(|e| error!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+
+        }
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                pixels.resize_surface(size.width, size.height);
+            }
+            for (x, k) in keys.iter() {
+                if input.key_pressed(*x) || input.key_held(*x){
+                    key = Some(*k);
+                }
+            }
+        }
+        if event == Event::MainEventsCleared {
+            window.request_redraw();
+        }
+    });
+}
